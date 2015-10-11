cf1fc28c3ec86b5e5588b3e5041e1ace51692f19
Kazusa Okuda <kazusa@klamath.jp>
2015-10-11 15:18:47 +0900

    Initial Commit

---
+++ hinayakuBotV2/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace hinayakuBotV2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Wake Up, HinayakuBot!");
+			var TwitterTask = Task.Run(() => TwitterStream ());
+			var BotTask = Task.Run (() => AI ());
+			System.Threading.Thread.Yield ();
+			t.Wait ();		
+		}
+
+
+	}
+}
---
+++ hinayakuBotV2/StreamObservable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CoreTweet;
+using Newtonsoft.Json;
+using System.Reactive.Linq;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Dynamic;
+using System.Reactive;
+using CoreTweet.Streaming;
+using CoreTweet.Streaming.Reactive;
+using Codeplex;
+using Microsoft.CSharp;
+using System.Reactive.Subjects;
+using System.Security.Cryptography;
+using System.Reactive.PlatformServices;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+
+namespace hinayakuBotV2
+{
+	public class StreamObservable
+	{
+		public StreamObservable ()
+		{
+		}
+	}
+}
+
